34c891cbbf7acd66d31edb522fe059a0bba3ea62
Elrashid <Elrashid@users.noreply.github.com>
2016-10-24 18:40:41 +0400

    Create PostStatus.cs

---
+++ WordPressPCL/Models/PostStatus.cs
@@ -0,0 +1,10 @@
+  static public class PostStatus
+    {
+        public static string Publish { get { return "publish"; } }
+        public static string Future { get { return "future"; } }
+        public static string Draft { get { return "draft"; } }
+        public static string Pending { get { return "pending"; } }
+        public static string Private { get { return "private"; } }
+ 
+    }
+}
