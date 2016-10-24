193ac2f33b532f9cb14f4c1597cc56a5ee5a34a6
Elrashid <Elrashid@users.noreply.github.com>
2016-10-24 18:42:58 +0400

    Update PostStatus.cs

--- WordPressPCL/Models/PostStatus.cs
+++ WordPressPCL/Models/PostStatus.cs
@@ -1,12 +1,27 @@
 namespace WordPressPCL.Models
-  {
-  static public class PostStatus
+{
+    static public class PostStatus
     {
-        public static string Publish { get { return "publish"; } }
-        public static string Future { get { return "future"; } }
-        public static string Draft { get { return "draft"; } }
-        public static string Pending { get { return "pending"; } }
-        public static string Private { get { return "private"; } }
- 
+        public static string Publish
+        {
+            get { return "publish"; }
+        }
+        public static string Future
+        {
+            get { return "future"; }
+        }
+        public static string Draft
+        {
+            get { return "draft"; }
+        }
+        public static string Pending
+        {
+            get { return "pending"; }
+        }
+        public static string Private
+        {
+            get { return "private"; }
+        }
+
     }
 }
