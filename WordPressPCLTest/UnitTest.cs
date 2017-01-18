849e3924fbbdbb5df1ca9e0291b6334b1084a009
Thomas Pentenrieder <pentenrieder@outlook.com>
2017-01-18 01:36:21 +0100

    Added UWP Test Project

---
+++ WordPressPCLTest/UnitTest.cs
@@ -0,0 +1,15 @@
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordPressPCLTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
