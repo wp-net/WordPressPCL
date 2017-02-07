1f82dd76fe164bbd063abcda72091a382366c4b2
Thomas Pentenrieder <pentenrieder@hotmail.com>
2017-02-07 13:54:24 +0100

    First test method
    added first test method to the unit test project

--- WordPressPCLTest/UnitTest.cs
+++ WordPressPCLTest/UnitTest.cs
@@ -1,15 +1,23 @@
-﻿
-using System;
+﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordPressPCL;
 
 namespace WordPressPCLTest
 {
     [TestClass]
     public class UnitTest1
     {
+
+
         [TestMethod]
-        public void TestMethod1()
+        public async void TestMethod1()
         {
+            // Initialize
+            var client = new WordPressClient("http://demo.wp-api.org/wp-json/wp/v2/");
+            Assert.IsNotNull(client);
+            // Posts
+            var posts = await client.ListPosts();
+            Assert.IsNotNull(posts);
         }
     }
 }
