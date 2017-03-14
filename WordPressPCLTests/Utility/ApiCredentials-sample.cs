c9e3cd57c983413f869786106616d233415dd84f
Thomas Pentenrieder <pentenrieder@hotmail.com>
2017-03-14 11:00:27 +0100

    Sample for ApiCredentials

---
+++ WordPressPCLTests/Utility/ApiCredentials-sample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordPressPCLTests.Utility
+{
+    public class ApiCredentials_sample
+    {
+        public static string WordPressUri = "http://yoursite.com/wp-json/";
+        public static string Username = "Name";
+        public static string Password = "password";
+    }
+}
