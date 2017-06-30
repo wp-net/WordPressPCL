65f060150e6415522c46e2ff4e3c73b67405d431
polushinmk <polushin@kell.ru>
2017-06-30 15:41:18 +0300

    Реорганизация структуры

---
+++ WordPressPCLTests/Utility/ApiCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordPressPCLTests.Utility
+{
+    public class ApiCredentials
+    {
+        public static string WordPressUri = "http://hanoi43.ru/wp-json/";
+        public static string Username = "Name";
+        public static string Password = "password";
+    }
+}
