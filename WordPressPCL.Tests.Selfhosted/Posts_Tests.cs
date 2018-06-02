79d402328bf9f01182df291cd306725710953a0d
Thomas Pentenrieder <pentenrieder@outlook.com>
2018-06-03 01:29:23 +0200

    Add Yoast Taxonomy Terms, update nuget version

--- WordPressPCL.Tests.Selfhosted/Posts_Tests.cs
+++ WordPressPCL.Tests.Selfhosted/Posts_Tests.cs
@@ -65,6 +65,19 @@ namespace WordPressPCL.Tests.Selfhosted
         }
 
         [TestMethod]
+        public async Task Posts_Read_Embedded()
+        {
+            var posts = await _client.Posts.Query(new PostsQueryBuilder()
+            {
+                PerPage = 10,
+                Page = 1,
+                Embed = true
+            }, false);
+            Assert.IsNotNull(posts);
+
+        }
+
+        [TestMethod]
         public async Task Posts_Update()
         {
             var testContent = "Test" + new Random().Next() ;
