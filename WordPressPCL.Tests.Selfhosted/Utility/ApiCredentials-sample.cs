9cd1e3e9d1e9b4e722cec96a692177bea2aeafce
Thomas Pentenrieder <pentenrieder@medialesson.de>
2020-05-08 19:48:59 +0200

    change admincredentials for running tests in ci/cd

--- WordPressPCL.Tests.Selfhosted/Utility/ApiCredentials-sample.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WordPressPCL.Tests.Selfhosted.Utility
-{
-    public class ApiCredentialsSample
-    {
-        public static string WordPressUri = "http://yoursite.com/wp-json/";
-        public static string Username = "Name";
-        public static string Password = "password";
-    }
-}
---
+++ WordPressPCL.Tests.Selfhosted/Utility/ApiCredentials.cs
@@ -0,0 +1,9 @@
+﻿namespace WordPressPCL.Tests.Selfhosted.Utility
+{
+    public class ApiCredentials
+    {
+        public static string WordPressUri = "http://localhost/wp-json/";
+        public static string Username = "wordpress";
+        public static string Password = "wordpress";
+    }
+}
