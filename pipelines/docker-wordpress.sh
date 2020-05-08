8d350c279cc80e3217d55c536497594c24e8b214
Thomas Pentenrieder <pentenrieder@medialesson.de>
2020-05-08 16:21:52 +0200

    install theme

--- pipelines/docker-wordpress.sh
+++ pipelines/docker-wordpress.sh
@@ -3,3 +3,5 @@ git clone https://github.com/kassambara/wordpress-docker-compose
 cd wordpress-docker-compose
 # Automatic installation of wordpress
 make autoinstall
+
+docker exec wordpress_wpcli wp theme install quark
