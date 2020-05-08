048a7ebdeee56892c17fa9907148803c4acf1fb0
Thomas Pentenrieder <pentenrieder@medialesson.de>
2020-05-08 16:24:25 +0200

    start container

--- pipelines/docker-wordpress.sh
+++ pipelines/docker-wordpress.sh
@@ -3,5 +3,6 @@ git clone https://github.com/kassambara/wordpress-docker-compose
 cd wordpress-docker-compose
 # Automatic installation of wordpress
 make autoinstall
+make start
 
 docker exec wordpress_wpcli wp theme install quark
