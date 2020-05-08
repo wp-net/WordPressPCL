7ef8c3e21a32161ebc6f5a91b6d79029f92d638e
Thomas Pentenrieder <pentenrieder@medialesson.de>
2020-05-08 17:00:47 +0200

    check if container is running

--- pipelines/docker-wordpress.sh
+++ pipelines/docker-wordpress.sh
@@ -3,6 +3,7 @@ git clone https://github.com/wp-net/wordpress-docker-compose
 cd wordpress-docker-compose
 # Automatic installation of wordpress
 make autoinstall
-make start
+
+curl -Is http://localhost | head -1
 
 docker exec wordpress_wpcli wp theme install quark
