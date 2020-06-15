620865e3a7da8c92bbb56e3074da1bdbaf6589fc
Gal Oron <tamper2@gmail.com>
2020-06-15 17:57:56 +0300

    added trash status (for post/page)

--- WordPressPCL/Models/Page.cs
+++ WordPressPCL/Models/Page.cs
@@ -66,7 +66,7 @@ namespace WordPressPCL.Models
         /// </summary>
         /// <remarks>
         /// Context: edit
-        /// One of: publish, future, draft, pending, private
+        /// One of: publish, future, draft, pending, private, trash
         /// </remarks>
         [JsonProperty("status")]
         public Status Status { get; set; }
