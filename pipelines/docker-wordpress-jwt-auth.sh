0182bf583aed9aded7e45192cc811a9ff73a7b22
Thomas Pentenrieder <pentenrieder@hotmail.com>
2020-09-21 10:41:57 +0200

    create pipeline for different plugin configs

--- pipelines/docker-wordpress-jwt-auth.sh
+++ pipelines/docker-wordpress-jwt-auth.sh
@@ -1,5 +1,5 @@
 # Download a wordpress docker-compose example
-git clone https://github.com/wp-net/wordpress-docker-compose
+git clone -b plugins/jwt-auth https://github.com/wp-net/wordpress-docker-compose
 cd wordpress-docker-compose
 # Automatic installation of wordpress
 make autoinstall
