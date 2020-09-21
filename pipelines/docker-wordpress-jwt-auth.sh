46206c4ecee9c4cf2d3b9456f90252cb77d5106f
Thomas Pentenrieder <pentenrieder@hotmail.com>
2020-09-21 10:59:00 +0200

    clone jwt-auth branch in differenent directory

--- pipelines/docker-wordpress-jwt-auth.sh
+++ pipelines/docker-wordpress-jwt-auth.sh
@@ -1,5 +1,6 @@
 #delete master repo
-rm -rf wordpress-docker-compose
+mkdir wordpress-docker-compose-jwt-auth
+cd wordpress-docker-compose-jwt-auth
 
 # Download a wordpress docker-compose example
 git clone -b plugins/jwt-auth https://github.com/wp-net/wordpress-docker-compose
