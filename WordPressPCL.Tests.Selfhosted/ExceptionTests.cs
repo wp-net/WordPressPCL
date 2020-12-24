eb6bf2e16e76458b8abce959868b3d96ea2057ee
Thomas Pentenrieder <pentenrieder@hotmail.com>
2020-12-24 14:48:16 +0100

    adjust exception test

--- WordPressPCL.Tests.Selfhosted/ExceptionTests.cs
+++ WordPressPCL.Tests.Selfhosted/ExceptionTests.cs
@@ -25,15 +25,11 @@ namespace WordPressPCL.Tests.Selfhosted
             // Initialize
             Assert.IsNotNull(_client);
             // Get settings without auth
-            try
+
+            await Assert.ThrowsExceptionAsync<WPException>(async () =>
             {
                 var settings = await _client.GetSettings();
-            }
-            catch (WPException wpex)
-            {
-                Assert.IsNotNull(wpex.RequestData);
-                Assert.AreEqual(wpex.RequestData.Name, "jwt_auth_bad_auth_header");
-            }
+            });
         }
 
         [TestMethod]
