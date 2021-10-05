a06ca973540607d4b868939dcc1b277c10545182
navjot50 <navjot.dcrust@gmail.com>
2021-10-05 20:26:50 +0200

    Changed the startup script to run for all versions of powershell

--- dev/containers-startup.sh
+++ dev/containers-startup.sh
@@ -4,5 +4,6 @@ docker-compose build
 docker-compose up --no-build -d
 docker build -t script ./wordpressbot/
 docker run -it --network=integration-tests-setup --name=script script
-docker stop selenium && docker rm selenium
+docker stop selenium 
+docker rm selenium
 docker rm script
