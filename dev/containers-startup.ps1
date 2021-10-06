3c9bbbb91ad81f4e30d160b39a431fe176f2a45c
Navjot <navjot.singh@scintec.com>
2021-10-06 11:28:29 +0200

    Added little delay in containers-startup script to allow for container initialization before automation script invocation

--- dev/containers-startup.ps1
+++ dev/containers-startup.ps1
@@ -1,6 +1,7 @@
 docker-compose rm -f
 docker-compose build
 docker-compose up --no-build -d
+Start-Sleep -s 10
 docker build -t script ./wordpressbot/
 docker run -it --network=integration-tests-setup --name=script script
 docker stop selenium 
--- dev/containers-startup.sh
+++ dev/containers-startup.sh
@@ -2,6 +2,7 @@
 docker-compose rm -f
 docker-compose build
 docker-compose up --no-build -d
+sleep 10
 docker build -t script ./wordpressbot/
 docker run -it --network=integration-tests-setup --name=script script
 docker stop selenium 
