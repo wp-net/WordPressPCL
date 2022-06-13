33790f71df92b775b4bc2043e16143e76f0185bb
Thomas Pentenrieder <pentenrieder@hotmail.com>
2022-06-13 20:39:01 +0200

    use dev docker for integration tests

--- pipelines/docker-wordpress.sh
+++ pipelines/docker-wordpress.sh
@@ -1,9 +1,2 @@
-# Download a wordpress docker-compose example
-git clone https://github.com/wp-net/wordpress-docker-compose
-cd wordpress-docker-compose
-# Automatic installation of wordpress
-make autoinstall
-
-docker ps
-
-curl -Is http://localhost | head -1
+cd dev
+docker-compose up
